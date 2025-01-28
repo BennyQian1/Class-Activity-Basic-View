9cefe5096187899b7f8ce2dc5e69825374afe181
BennyQian1 <tuq28313@temple.edu>
2025-01-28 12:13:27 -0500

    -Added a CLICKME BUTTON -Added a function to the Button

--- app/src/main/java/edu/temple/classactivity/MainActivity.kt
+++ app/src/main/java/edu/temple/classactivity/MainActivity.kt
@@ -1,16 +1,29 @@
 package edu.temple.classactivity
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var clickmebutton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        clickmebutton = findViewById(R.id.button3) //or you can do it like this "findViewById(R.id.**itemid**)setOnClickListener{view: View -> (view as Button).text = "Button CLicked!"}
+        clickmebutton.setOnClickListener{
+            clickmebutton.text = "Button Clicked"
+        }
+
+        (findViewById<Button>(R.id.button2)!!).setOnClickListener {
+            (it as Button).text = "Button Clicked"
+        }
 
     }
 }
